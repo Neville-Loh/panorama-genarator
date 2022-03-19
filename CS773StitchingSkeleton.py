b70d0333f8cc72817f73d4399648219c33e0e134
nber106 <nber106@aucklanduni.ac.nz>
2022-03-19 21:17:52 +1300

    comparing left and right images

--- CS773StitchingSkeleton.py
+++ CS773StitchingSkeleton.py
@@ -116,50 +116,72 @@ def main():
     # Use a 3x3 averaging as an initial blurring
 
     fig1, axs1 = pyplot.subplots(4, 4, figsize=(12, 10))
+    axs1[0][0].set_title('Original Image Greyscale')
     axs1[0][0].imshow(px_array_left_original, cmap='gray')
+    axs1[0][1].set_title('Smoothed and Stretched to full 8 bit range')
     axs1[0][1].imshow(px_array_left, cmap='gray')
 
     # Step 2
     # Implement e.g. Sobel filter in x and y, (The gradiate)  for X and Y derivatives
 
-    ix, iy = sobel(np.array(px_array_left), image_width, image_height)
-    axs1[0][2].imshow(ix, cmap='gray')
-    axs1[0][3].imshow(iy, cmap='gray')
+    ix_left, iy_left = sobel(np.array(px_array_left), image_width, image_height)
+    ix_right, iy_right = sobel(np.array(px_array_right), image_width, image_height)
+    axs1[0][2].set_title('Sobel filter for X derivative')
+    axs1[0][2].imshow(ix_left, cmap='gray')
+    axs1[0][3].set_title('Sobel filter for Y derivative')
+    axs1[0][3].imshow(iy_left, cmap='gray')
 
     # Step 3
     # compute the square derivatives and the product of the mixed derivatives, smooth them,
     # Play with different size of gaussian window (5x5, 7x7, 9x9)
 
-    ix2, iy2, ixiy = t = get_square_and_mixed_derivatives(ix, iy)
-    axs1[1][0].imshow(ix2, cmap='gray')
-    axs1[1][1].imshow(iy2, cmap='gray')
-    axs1[1][2].imshow(ixiy, cmap='gray')
+    ix2_left, iy2_left, ixiy_left = t_left = get_square_and_mixed_derivatives(ix_left, iy_left)
+    ix2_right, iy2_right, ixiy_right = t_right = get_square_and_mixed_derivatives(ix_right, iy_right)
+    axs1[1][0].imshow(ix2_left, cmap='gray')
+    axs1[1][1].imshow(iy2_left, cmap='gray')
+    axs1[1][2].imshow(ixiy_left, cmap='gray')
     axs1[1][3].axis('off')
 
     # gaussian blur
-    ix2_blur, iy2_blur, ixiy_blur = [compute_gaussian_averaging(img, image_width, image_height) for img in t]
+    ix2_blur_left, iy2_blur_left, ixiy_blur_left = [compute_gaussian_averaging(img, image_width, image_height) for img in t_left]
+    ix2_blur_right, iy2_blur_right, ixiy_blur_right = [compute_gaussian_averaging(img, image_width, image_height) for img in t_right]
 
-    axs1[2][0].imshow(ix2_blur, cmap='gray')
-    axs1[2][1].imshow(iy2_blur, cmap='gray')
-    axs1[2][2].imshow(ixiy_blur, cmap='gray')
+    axs1[2][0].imshow(ix2_blur_left, cmap='gray')
+    axs1[2][1].imshow(iy2_blur_left, cmap='gray')
+    axs1[2][2].imshow(ixiy_blur_left, cmap='gray')
     # axs1[2][3].axis('off')
 
     # Choose a Harris constant between 0.04 and 0.06
 
     # 5 extract Harris corner as (x,y) tuples in a data structure, which is sorted according to the strength of the
     # Harris response function C, sorted list of tuples
-    corner_img_array = get_image_cornerness(ix2_blur, iy2_blur, ixiy_blur, 0.04)
-    axs1[2][3].imshow(corner_img_array, cmap='gray')
+    corner_img_array_left = get_image_cornerness(ix2_blur_left, iy2_blur_left, ixiy_blur_left, 0.04)
+    corner_img_array_right = get_image_cornerness(ix2_blur_right, iy2_blur_right, ixiy_blur_right, 0.04)
+    axs1[2][3].imshow(corner_img_array_left, cmap='gray')
 
     # 5.5 non-max suppression
-    corner_img_array = bruteforce_non_max_suppression(corner_img_array, window_size=3)
+    corner_img_array_left = bruteforce_non_max_suppression(corner_img_array_left, window_size=3)
+    corner_img_array_right = bruteforce_non_max_suppression(corner_img_array_right, window_size=3)
 
     # 6 Prepare n=1000 strongest conner per image
-    pq_1000_coor = [(corner.y, corner.x) for corner in heapq.nsmallest(1000, get_all_corner(corner_img_array))]
+    pq_1000_coor_left = [(corner.y, corner.x) for corner in heapq.nsmallest(1000, get_all_corner(corner_img_array_left))]
+    pq_1000_coor_right = [(corner.y, corner.x) for corner in heapq.nsmallest(1000, get_all_corner(corner_img_array_right))]
 
-    # some simple visualizations
     axs1[3][0].imshow(px_array_left_original, cmap='gray')
-    axs1[3][0].scatter(*zip(*pq_1000_coor), s=1, color='r')
+    axs1[3][0].scatter(*zip(*pq_1000_coor_left), s=1, color='r')
+
+    axs1[3][1].imshow(px_array_right_original, cmap='gray')
+    axs1[3][1].scatter(*zip(*pq_1000_coor_right), s=1, color='r')
+
+    pyplot.show()
+
+    fig1, axs1 = pyplot.subplots(1, 2)
+
+    axs1[0].imshow(px_array_left_original, cmap='gray')
+    axs1[0].scatter(*zip(*pq_1000_coor_left), s=1, color='r')
+
+    axs1[1].imshow(px_array_right_original, cmap='gray')
+    axs1[1].scatter(*zip(*pq_1000_coor_right), s=1, color='r')
 
     pyplot.show()
 
