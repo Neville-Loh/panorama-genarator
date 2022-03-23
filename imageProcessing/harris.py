8a9fec6a4926b175da42de0392a70181c4cf275f
Neville Loh <48974742+Neville-Loh@users.noreply.github.com>
2022-03-23 23:12:02 +1300

    updated documentation and refactored code

--- imageProcessing/harris.py
+++ imageProcessing/harris.py
@@ -1,155 +1,151 @@
-from typing import List, Tuple, Optional, Type, Any
-from matplotlib import pyplot as plt
 import numpy as np
 import heapq
-
 import imageProcessing.smoothing as IPSmooth
-import imageProcessing.utilities as IPUtils
-import imageProcessing.convolve2D as IPConv2D
+from typing import List, Tuple, Optional, Type
+from matplotlib import pyplot as plt
+from imageProcessing.corner import Corner, get_all_corner
+from imageProcessing.harris_util import sobel, compute_gaussian_averaging
 
-from imageProcessing.convolve2D import computeSeparableConvolution2DOddNTapBorderZero
-from imageProcessing.harris_util import get_gaussian_kernel
+"""
+Harris corner detection
 
+Based file
+"""
+
+
+
+# Default image type is np array.
 ImageArray = np.ndarray
-
-
-class Corner:
-    def __init__(self, index: Tuple[int, int], cornerness: float):
-        self.y, self.x = index
-        self.cornerness = cornerness
-
-    def __lt__(self, other):
-        return self.cornerness > other.cornerness
-
-    def __eq__(self, other):
-        return self.cornerness == other.cornerness
-
-    def __str__(self) -> str:
-        return str((self.x, self.y, self.cornerness))
-
-    def __repr__(self):
-        return str(self)
 
 
 def compute_harris_corner(img_original: List[List[int]],
                           n_corner: Optional[int] = 5,
                           alpha: Optional[float] = 0.04,
-                          gaussian_window_size: Optional[int] = 3,
+                          gaussian_window_size: Optional[int] = 5,
                           plot_image: Optional[bool] = False) \
-        -> List[Corner]:
+        -> List[Type[Corner]]:
     """
     Compute the harris corner for the picture
     return the corner activated value in decreasing value
     optional parameters
         n_corner: Optional[int], default =5,
         alpha: Optional[float], default =0.04,
-        gaussian_window_size: Optional[int], default =3,
+        gaussian_window_size: Optional[int], default =5,
         plot_image: Optional[bool], default =False)
 
     """
 
-    # step 1
+    # Apply Gaussian filter, blur and smoothing for the input image
     np_original = np.array(img_original)
     height, width = np.shape(np_original)
-    px_array_left = IPSmooth.computeGaussianAveraging3x3(img_original, width, height)
+    px_array = IPSmooth.computeGaussianAveraging3x3(img_original, width, height)
 
-    # Step 2
-    # Implement e.g. Sobel filter in x and y, (The gradient)  for X and Y derivatives
-    ix, iy = sobel(np_original)
+    # Apply Sobel filters in x and y direction to compute the gradient, X and Y derivatives
+    ix, iy = sobel(px_array)
 
-    # Step 3
-    # compute the square derivatives and the product of the mixed derivatives, smooth them,
-    # Play with different size of gaussian window (5x5, 7x7, 9x9)
+    # Compute the square derivatives and the product of the mixed derivatives, smooth them,
+    result_tuple = get_square_and_mixed_derivatives(ix, iy)
 
-    ix2, iy2, ixiy = t_left = get_square_and_mixed_derivatives(ix, iy)
+    # Apply Gaussian blur with input windows size, if no window size is given, a default size of 5 by 5 is used
+    ix2_blur_left, iy2_blur_left, ixiy_blur_left \
+        = [compute_gaussian_averaging(img, windows_size=gaussian_window_size) for img in result_tuple]
 
-    # gaussian blur
-    ix2_blur_left, iy2_blur_left, ixiy_blur_left = [compute_gaussian_averaging(img, windows_size=gaussian_window_size) for img in t_left]
-
-
-    # Choose a Harris constant between 0.04 and 0.06
-
-    # 5 extract Harris corners as (x,y) tuples in a data structure, which is sorted according to the strength of the
-    # Harris response function C, sorted list of tuples
+    # Compute the Harris response for each pixel
     corner_img_array = get_image_cornerness(ix2_blur_left, iy2_blur_left, ixiy_blur_left, alpha)
 
-    # 5.5 non-max suppression
+    # Apply local non-max suppression for each piexels
     corner_img_array = bruteforce_non_max_suppression(corner_img_array, window_size=3)
 
-    # 6 Prepare n=1000 strongest conner per image
+    # Prepare n=1000 strongest conner per image
     pq_n_best_corner = heapq.nsmallest(n_corner, get_all_corner(corner_img_array))
 
-    pq_n_best_corner_coor = [(corner.x, corner.y) for corner in pq_n_best_corner]
-
+    # Plot the image if optional argument plot_image is true
     if plot_image:
         plt.figure(figsize=(20, 18))
         plt.gray()
         plt.imshow(img_original)
-        plt.scatter(*zip(*pq_n_best_corner_coor), s=1, color='r')
+        plt.scatter(*zip(*[(corner.x, corner.y) for corner in pq_n_best_corner]), s=1, color='r')
         plt.axis('off')
         plt.show()
+
+    # Return List of Corner as heap
     return pq_n_best_corner
 
 
-def sobel(px_array: ImageArray) -> Tuple[ImageArray, ImageArray]:
-    """
-    Apply sobel filter using 2D convolution
-    Returns: image i_x, i_y
-    """
-    image_height, image_width = np.shape(px_array)
-    ix_kernel = ([-1, 0, 1], [1, 2, 1])
-    iy_kernel = ([1, 2, 1], [-1, 0, 1])
-
-    i_x = computeSeparableConvolution2DOddNTapBorderZero(px_array, image_width, image_height,
-                                                         kernelAlongX=ix_kernel[0],
-                                                         kernelAlongY=ix_kernel[1])
-    i_y = computeSeparableConvolution2DOddNTapBorderZero(px_array, image_width, image_height,
-                                                         kernelAlongX=iy_kernel[0],
-                                                         kernelAlongY=iy_kernel[1])
-    return np.array(i_x), np.array(i_y)
-
-
 def get_square_and_mixed_derivatives(i_x: ImageArray, i_y: ImageArray) -> Tuple[ImageArray, ImageArray, ImageArray]:
+    """Compute the square and mixed derivatives of the image
+    Parameters
+    ----------
+    i_x : ImageArray
+        An ImageArray that contain derivatives of x direction
+    i_y : ImageArray
+        An ImageArray that contain derivatives of y direction
+
+    Returns
+    -------
+    Tuple[ImageArray, ImageArray, ImageArray]
+        A tuple containing 3 result, the square of x derivatives, the square of x derivatives, and the product of the
+        x y derivatives.
+    """
     return np.square(i_x), np.square(i_y), np.multiply(i_x, i_y)
 
 
-def compute_gaussian_averaging(pixel_array: ImageArray, windows_size: Optional[int] = 5) -> ImageArray:
-    image_height, image_width = np.shape(pixel_array)
-    kernel = get_gaussian_kernel(windows_size, sigma=1)
-    averaged = IPConv2D.computeSeparableConvolution2DOddNTapBorderZero(
-        pixel_array.tolist(), image_width, image_height, kernel)
-
-    return np.array(averaged)
-
-
 def get_image_cornerness(ix2: ImageArray, iy2: ImageArray, ixiy: ImageArray, alpha: float) -> ImageArray:
+    """Compute the gaussian 1D kernel given the sigma as a constants
+    Parameters
+    ----------
+    ix2 : ImageArray
+        An ImageArray that contain squares derivatives of x direction
+    iy2 : ImageArray
+        An ImageArray that contain squares derivatives of y direction
+    ixiy : ImageArray
+        An ImageArray that contain product of the derivative
+    alpha : int
+
+
+    Returns
+    -------
+    ImageArray
+        An ImageArray, where each coordinate contains the Harris response of each pixel.
+    """
     result = np.multiply(ix2, iy2) - np.square(ixiy) - (np.square(np.add(ix2, iy2)) * alpha)
     return result
 
 
-def get_all_corner(img: ImageArray) -> List[Type[Corner]]:
-    pq = []
-    for index, val in np.ndenumerate(img):
-        heapq.heappush(pq, Corner(index, val))
-    return pq
-
-
 def bruteforce_non_max_suppression(input_img: ImageArray, window_size: Optional[int] = 3) -> ImageArray:
+    """Applied local non max suppression for the iamge
+    A n by n pixel windows is iterated over the image while the center pixel is suppressed if
+    it is not a local maximum compare to its nearest 8 neighbour.
+
+    Parameters
+    ----------
+    input_img : ImageArray
+        The input image array before suppression
+    window_size :  Optional[int]
+        Suppression windows size, only works for odd number, if none are supplied, a default value of 3 is used
+
+    Returns
+    -------
+    ImageArray
+        An ImageArray after suppression
+    """
     height, width = np.shape(input_img)
     center_window_index = window_size ** 2 // 2
     input_img = input_img.flatten()
 
-    # Create window
+    # Create sliding window that contain correct index from the input image
     window = []
     for i in range(window_size):
         window += [i * width + j for j in range(window_size)]
     window = np.array(window)
 
     row = 0
+
+    # Move the windows from top left to bottom right until the end
     while window[-1] < len(input_img):
         max_index = np.argmax(input_img[window])
 
-        # suppress non max to 0 if nearest neighbour have a higher activation
+        # suppress non max to 0 if nearest (n*n - 1) neighbours have a higher activation
         if max_index != center_window_index:
             input_img[window[center_window_index]] = 0
 
--- imageProcessing/harris_util.py
+++ imageProcessing/harris_util.py
@@ -1,13 +1,85 @@
-from typing import List
+from typing import List, Tuple, Optional
 
 import numpy as np
-import typing
+from imageProcessing.convolve2D import computeSeparableConvolution2DOddNTapBorderZero
+import imageProcessing.convolve2D as IPConv2D
+"""
+Utility class contain helper function for computing harris corner
+
+"""
 
 
-def get_gaussian_kernel(window_size: int, sigma: int):
+# Default image type is np array.
+ImageArray = np.ndarray
+
+
+def sobel(px_array: ImageArray) -> Tuple[ImageArray, ImageArray]:
+    """Compute the gaussian 1D kernel given the sigma as a constants
+    Parameters
+    ----------
+    px_array : ImageArray
+        generate a window_size by window_size filter
+
+    Returns
+    -------
+    Tuple[ImageArray, ImageArray]
+        A tuple containing 2 result image for along x and y direction
+    """
+    image_height, image_width = np.shape(px_array)
+    ix_kernel = ([-1, 0, 1], [1, 2, 1])
+    iy_kernel = ([1, 2, 1], [-1, 0, 1])
+
+    i_x = computeSeparableConvolution2DOddNTapBorderZero(px_array, image_width, image_height,
+                                                         kernelAlongX=ix_kernel[0],
+                                                         kernelAlongY=ix_kernel[1])
+    i_y = computeSeparableConvolution2DOddNTapBorderZero(px_array, image_width, image_height,
+                                                         kernelAlongX=iy_kernel[0],
+                                                         kernelAlongY=iy_kernel[1])
+    return np.array(i_x), np.array(i_y)
+
+
+def compute_gaussian_averaging(pixel_array: ImageArray, windows_size: Optional[int] = 5) -> ImageArray:
+    """Compute the gaussian 1D kernel given the sigma as a constants
+    Parameters
+    ----------
+    pixel_array : ImageArray
+        A 2 dimensional imageArray that contain
+    windows_size : Optional[int]
+        the default windows size used for gaussian averaging, if none are supplied, a default size of 5 will be used
+
+    Returns
+    -------
+    ImageArray
+        The result image after gaussian filter is applied
+    """
+    image_height, image_width = np.shape(pixel_array)
+    kernel = get_gaussian_kernel(windows_size, sigma=1)
+    averaged = IPConv2D.computeSeparableConvolution2DOddNTapBorderZero(
+        pixel_array.tolist(), image_width, image_height, kernel)
+
+    return np.array(averaged)
+
+
+def get_gaussian_kernel(window_size: int, sigma: float, offset: Optional[float] = 0.0) -> List[float]:
+    """Compute the gaussian 1D kernel given the sigma as a constants
+
+    Parameters
+    ----------
+    window_size : int
+        generate a window_size by window_size filter
+    sigma : float
+        the steepness of the fall of the Gaussian
+    offset : float Optional
+        the offset of the gaussian windows
+
+    Returns
+    -------
+    list[float]
+        a list of kernel of size window_size
+    """
     x, y = np.meshgrid(np.linspace(-1, 1, window_size), np.linspace(-1, 1, window_size))
     d = np.sqrt(x * x + y * y)
-    mu = 0.0
+    mu = offset
     g = np.exp(-((d - mu) ** 2 / (2.0 * sigma ** 2)))
 
     result: np.ndarray = np.array([0] * len(g[0]))
